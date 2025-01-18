d5b162da5a195f65feccfd73b44578f1fb4c82d4
voxty <135042947+voxty@users.noreply.github.com>
2025-01-17 20:00:25 -0500

    Create iniconfig.cs

---
+++ [C#]/iniconfig.cs
@@ -0,0 +1,38 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System;
+
+public static class ConfigLoader
+{
+    public static string LicenseKey { get; private set; }
+
+
+    public static string ResourceName = API.GetCurrentResourceName();
+
+    static ConfigLoader()
+    {
+        LoadConfig();
+    }
+
+    public static void LoadConfig()
+    {
+        try
+        {
+            string configContent = API.LoadResourceFile(API.GetCurrentResourceName(), "config.ini");
+
+            if (!string.IsNullOrEmpty(configContent))
+            {
+                Config config = new Config(configContent);
+                LicenseKey = config.Get("LicenseKey", "none");
+            }
+            else
+            {
+                Console.WriteLine("Config file content is empty or not found.");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error loading config: {e.Message}");
+        }
+    }
+}
